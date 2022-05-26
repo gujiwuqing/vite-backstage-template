26970a7510c7c1be0adfc76e9ca0ed85fabe2265
孤寂无情 <15279279713@163.com>
2022-05-26 11:29:27 +0800

    feat: 添加tailwindcss 3.0

--- src/layout/BaseHeader/index.tsx
+++ src/layout/BaseHeader/index.tsx
@@ -2,35 +2,35 @@ import React from 'react';
 import style from './index.module.less';
 import {GithubOutlined, MenuFoldOutlined, MenuUnfoldOutlined} from '@ant-design/icons';
 import ThemeColor from './ThemeColor';
-import {useAtom} from 'jotai'
-import {collapsedAtom} from "@/store";
-import './index.less'
+import {useAtom} from 'jotai';
+import {collapsedAtom} from '@/store';
+import './index.less';
 
 const BaseHeader = () => {
-  const [collapsed, setCollapsed] = useAtom(collapsedAtom)
-  const onClick = () => {
-    setCollapsed(!collapsed)
-  };
-  return (
-    <header className={style.header}>
-      <div>
-        <span style={{ marginRight: 10 }}>后台管理系统</span>
-        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-          className: 'trigger',
-          onClick,
-        })}
-      </div>
-      <div className={style.header_right}>
-        <ThemeColor/>
-        <GithubOutlined
-          className={style.icon}
-          onClick={() => {
-            window.location.href = 'https://github.com/gujiwuqing/vite-backstage-template';
-          }}
-        />
-      </div>
-    </header>
-  );
+    const [collapsed, setCollapsed] = useAtom(collapsedAtom);
+    const onClick = () => {
+        setCollapsed(!collapsed);
+    };
+    return (
+        <header className={style.header}>
+            <div className='flex items-center'>
+                <span className="mr-3">后台管理系统</span>
+                {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                    className: 'trigger',
+                    onClick,
+                })}
+            </div>
+            <div className={style.header_right}>
+                <ThemeColor/>
+                <GithubOutlined
+                    className={style.icon}
+                    onClick={() => {
+                        window.location.href = 'https://github.com/gujiwuqing/vite-backstage-template';
+                    }}
+                />
+            </div>
+        </header>
+    );
 };
 
 export default BaseHeader;
