2446d56d9961aaaaf90cd2c1dc458daa5b63cf48
孤寂无情 <15279279713@163.com>
2022-08-02 15:22:07 +0800

    升级成vite3.0，实现antd按需加载

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.less';
+// import 'antd/dist/antd.less';
 import {ConfigProvider} from 'antd';
-import 'antd/dist/antd.variable.min.css';
+// import 'antd/dist/antd.variable.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,35 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import usePluginImport from 'vite-plugin-importer';
+import {resolve} from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    // 路径别名
-    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json', '.sass', '.scss'], // 忽略输入的扩展名
-    alias: [
-      { find: /^~/, replacement: '' },
-      { find: '@', replacement: resolve(__dirname, 'src') },
-    ],
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
+    plugins: [react(),
+        usePluginImport({
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true,
+        })],
+    resolve: {
+        // 路径别名
+        extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json', '.sass', '.scss'], // 忽略输入的扩展名
+        alias: [
+            {find: /^~/, replacement: ''},
+            {find: '@', replacement: resolve(__dirname, 'src')},
+        ],
     },
-  },
-  server: {
-    open: true
-  }
-})
+    css: {
+        preprocessorOptions: {
+            less: {
+                javascriptEnabled: true,
+            },
+        },
+    },
+    server: {
+        open: true,
+        port: 3000,
+    }
+});
 
 
